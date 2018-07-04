144db88887b1248223e55e64737586acdcab6aff
Stuart Wakefield <me@stuartwakefield.co.uk>
2018-07-04 17:59:00 +0100

    Refactoring and tidy up

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { Server } from './lib/server/index';
 
 const server = new Server({
-    port: process.env.npm_package_config_port
+    port: process.env.npm_package_config_port,
+    baseUrl: process.env.npm_package_config_baseUrl
 });
 server.start();
