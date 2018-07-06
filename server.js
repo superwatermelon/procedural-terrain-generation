c2b670f3024ed4ee6348183986582c911bb43ddc
Stuart Wakefield <me@stuartwakefield.co.uk>
2018-07-06 14:30:55 +0100

    Enforce SSL connection

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import { Server } from './lib/server/index';
 
 const port = process.env.PORT || process.env.npm_package_config_port || 3000;
+const sslOnly = process.env.SSL_ONLY || process.env.npm_package_config_sslOnly;
 
 const server = new Server({
-    port
+    port,
+    sslOnly: sslOnly && sslOnly !== 'false'
 });
 server.start();
