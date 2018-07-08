42a09f2641ed96197c3886373ebbbf08c59e7645
Stuart Wakefield <me@stuartwakefield.co.uk>
2018-07-08 14:34:56 +0100

    Use base URL in canonicalization

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import { Server } from './lib/server/index';
 
-const port = process.env.PORT || process.env.npm_package_config_port || 3000;
-const sslOnly = process.env.SSL_ONLY || process.env.npm_package_config_sslOnly;
+const DEFAULT_PORT = 3000;
+const DEFAULT_BASE_URL = '';
+const DEFAULT_SSL_ONLY = false;
+
+const port = process.env.PORT || process.env.npm_package_config_port || DEFAULT_PORT;
+const sslOnly = process.env.SSL_ONLY || process.env.npm_package_config_sslOnly || DEFAULT_SSL_ONLY;
+const baseUrl = process.env.BASE_URL || process.env.npm_package_config_baseUrl || DEFAULT_BASE_URL;
 
 const server = new Server({
     port,
-    sslOnly: sslOnly && sslOnly !== 'false'
+    sslOnly: sslOnly && sslOnly !== 'false',
+    baseUrl
 });
 server.start();
